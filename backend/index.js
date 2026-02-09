45c1624c8ff88f61b46a2ea7a8cce277aeacf066
Dipanshu Raj <rajdipanshu180@gmail.com>
2026-02-09 18:36:42 +0530

    removed render speed up

--- backend/index.js
+++ backend/index.js
@@ -110,21 +110,21 @@ import axios from "axios";
 const app = express();
 const server = http.createServer(app);
 
-const url = `https://codesync-492y.onrender.com/`;
-const interval = 30000;
+// const url = `https://codesync-492y.onrender.com/`;
+// const interval = 30000;
 
-function reloadWebsite() {
-  axios
-    .get(url)
-    .then((response) => {
-      console.log("website reloded");
-    })
-    .catch((error) => {
-      console.error(`Error : ${error.message}`);
-    });
-}
+// function reloadWebsite() {
+//   axios
+//     .get(url)
+//     .then((response) => {
+//       console.log("website reloded");
+//     })
+//     .catch((error) => {
+//       console.error(`Error : ${error.message}`);
+//     });
+// }
 
-setInterval(reloadWebsite, interval);
+// setInterval(reloadWebsite, interval);
 
 
 const io = new Server(server, {
